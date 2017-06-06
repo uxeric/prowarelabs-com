f2defa5febc957d1d0299ade6639473c58751b73
uxeric <eric@uxeric.com>
2017-06-05 20:27:23 -0400

    Adds exception for PDFs

--- molecules/why-proinspector.php
+++ molecules/why-proinspector.php
@@ -3,23 +3,23 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12 text-center">
-                <h2 class="heading"><span>All Our</span> Products <span class="divider-center"></span></h2>
+                <h2 class="heading"><span>Why Choose</span> ProInspector <span class="divider-center"></span></h2>
             </div>
             <div class="icon_wrapclearfix">
                 <div class="col-sm-4 icon_box text-center margin_tophalf wow fadeInUp" data-wow-delay="500ms">
                     <i class="icon-global"></i>
-                    <h4 class="text-capitalize bottom20 margin10">ProFranchise</h4>
-                    <p class="no_bottom">Keep away from people who try to belittle your ambitions. Small people always do that but the really great.</p>
+                    <h4 class="text-capitalize bottom20 margin10">Easy-to-use</h4>
+                    <p class="no_bottom">Making the change to ProInspector is nearly effortless and allows you to increase efficiency almost immediately.</p>
                 </div>
                 <div class="col-sm-4 icon_box text-center margin_tophalf wow fadeInUp" data-wow-delay="500ms">
                     <i class="icon-laptop"></i>
-                    <h4 class="text-capitalize bottom20 margin10">ProInspector</h4>
-                    <p class="no_bottom">Keep away from people who try to belittle your ambitions. Small people always do that but the really great.</p>
+                    <h4 class="text-capitalize bottom20 margin10">Custom Reports</h4>
+                    <p class="no_bottom">Generate custom internal reports including employee performance, year end, as well as reports for council.</p>
                 </div>
                 <div class="col-sm-4 icon_box text-center margin_tophalf wow fadeInUp" data-wow-delay="500ms">
                     <i class="icon-globe"></i>
-                    <h4 class="text-capitalize bottom20 margin10">ProLearning</h4>
-                    <p class="no_bottom">Keep away from people who try to belittle your ambitions. Small people always do that but the really great.</p>
+                    <h4 class="text-capitalize bottom20 margin10">Import from Other Applications</h4>
+                    <p class="no_bottom">Looking to make the change? We can import permits from other applications.</p>
                 </div>
             </div>
         </div>
