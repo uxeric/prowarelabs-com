8bb7422f9d6991ea54b4980c86fd574a3d9231b0
uxeric <eric@uxeric.com>
2017-06-05 20:28:04 -0400

    Improves 404 styling

--- views/404.php
+++ views/404.php
@@ -5,11 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 page-content">
-                <h1>404 Error</h1>
-                <p>We offer the most complete house renovating services in the country</p>
-                <div class="page_nav">
-                    <span>You are here:</span> <a href="index.html">Home</a> <span><i class="fa fa-angle-double-right"></i>404</span>
-                </div>
+
             </div>
         </div>
     </div>
@@ -25,7 +21,7 @@
                     <h1>404</h1>
                     <h2>404</h2>
                 </div>
-                <p class="heading_space">We are sorry, the page you want isn’t here anymore. May be one of the links below can help !</p>
+                <p class="heading_space">We are sorry, the page you were searching for no longer exists. Maybe one of the links below can help.</p>
                 <a href="index.html" class="btn_common blue border_radius wow fadeIn" data-wow-delay="400ms">Go to Homepage</a>
                 <a href="index.html" class="btn_common yellow border_radius wow fadeIn" data-wow-delay="400ms">Get a quote</a>
             </div>
