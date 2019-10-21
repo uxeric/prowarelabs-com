1a9574490ef7b78bf9b4872abf92d701aa264f98
Eric <eric@uxeric.com>
2019-10-21 17:12:15 -0400

    Fixes Inquiry Type on contact form
    The animation as well as the function

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,31 +1,51 @@
 /* Contact Us */
 $('.address').click(function(){
-    $(this).addClass('active').siblings().removeClass('active');
-    if ( $(this).hasClass('type-contact') )
-    {
-        $("#type").val("General Inquiry");
-    }
-    else if ( $(this).hasClass('type-demo') )
-    {
-        $("#type").val("Demo Request");
-    }
-    else if ( $(this).hasClass('type-support') )
-    {
-        $("#type").val("Technical Support");
-    }
+  $(this).addClass('active').siblings().removeClass('active');
+  if ( $(this).hasClass('type-contact') )
+  {
+      $("#type").val("General Inquiry");
+  }
+  else if ( $(this).hasClass('type-demo') )
+  {
+      $("#type").val("Demo Request");
+  }
+  else if ( $(this).hasClass('type-support') )
+  {
+      $("#type").val("Technical Support");
+  }
 });
 
 $( document ).ready(function() {
-    if ( $('.address.active').hasClass('type-contact') )
-    {
-        $("#type").val("General Inquiry");
+  let contact_type = getParameter('type');
+
+  if ( contact_type === 'contact' )
+  {
+      $("#type").val("General Inquiry");
+      $('.type-contact').addClass('active');
     }
-    else if ( $('.address.active').hasClass('type-demo') )
+    else if ( contact_type === 'demo' )
     {
-        $("#type").val("Demo Request");
+      $("#type").val("Demo Request");
+      $('.type-demo').addClass('active');
     }
-    else if ( $('.address.active').hasClass('type-support') )
+    else if ( contact_type === 'support' )
     {
-        $("#type").val("Technical Support");
-    }
+      $("#type").val("Technical Support");
+      $('.type-support').addClass('active');
+  }
 });
+
+function getParameter(parameterName) {
+// https://stackoverflow.com/questions/5448545/how-to-retrieve-get-parameters-from-javascript/
+
+var result = null,
+    tmp = [];
+location.search
+    .substr(1)
+    .split("&")
+    .forEach(function (item) {
+      tmp = item.split("=");
+      if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    });
+return result;
+}
